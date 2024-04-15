d21e284372494d90c26777710b95c6e7f7ba8012
Arthur Cavalleiro <98990206+Arthur-Cavalleiro@users.noreply.github.com>
2024-04-15 17:14:21 -0300

    Added coments for program.c

--- program.c
+++ program.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+
+
+/*
+  A: N = entrada
+  B: resultado = 1
+  C: i = 1
+  D: resultado = resultado * i
+  E: i = i+1
+  F: print(resultado)
+*/ 
+
 int fatorialMonolitico(int n) {
     if (n < 0) {
         return -1;
@@ -14,20 +25,58 @@ inicio:
     if (i <= n)
         goto inicio;
     return resultado;
+  /*
+    1: Faça A va_para 2
+    2: Se n < 0 então va_para Rx senão va_para 3
+    3: Faça B va_para 4
+    4: Faça C va_para 5
+    5: Faça D va_para 6
+    6: Faça E va_para 7
+    7: Se i <= n então va_para 8 senão va_para 5
+    8: Faça F va_para Rx(✔)
+    Rx: print(error)
+  */
 }
 
+
+/*
+  A: n = entrada
+  B: resultado = 1
+  C: i = 1
+  D: resultado = resultado * i
+  E: i = i+1
+  F: print(resultado)
+*/
 int fatorialIterativo(int n) {
     if (n < 0) {
         return -1;
     }
     
     int resultado = 1;
-    for (int i = 1; i <= n; i++) {
+    int i = 1;
+    for (i; i <= n; i++) {
         resultado *= i;
     }
     return resultado;
+  /*
+    1: Faça A va_para 2
+    2: Se n < 0 então va_para Rx senão va_para 3
+    3: Faça B va_para 4
+    4: Faça C va_para 5
+    5: Se i <= n então va_para 6 senão va_para 8 
+    6: Faça D va_para 7
+    7: Faça E va_para 5
+    8: Faça F va_para Rx(✔)
+    Rx: print(error)
+  */
 }
 
+
+/*
+  A: n = entrada
+  B: return 1
+  C: return n * func(n-1)
+*/
 int fatorialRecursivo(int n) {
     if (n < 0) {
         return -1;
@@ -36,6 +85,15 @@ int fatorialRecursivo(int n) {
         return 1;
     else
         return n * fatorialRecursivo(n - 1);
+  /*
+    1: Faça A va_para 2
+    2: Se n < 0 então va_para Rx senão va_para 3
+    3: Se n == 0 então va_para 4 senão va_para 5
+    4: Faça B va_para 6
+    5: Faça C va_para 6
+    6: Completo Rx(✔)
+    Rx: print(error)
+  */
 }
 
 int main() {
