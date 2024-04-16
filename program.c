2921722f10bdd7eeaee01ab9478ccdfaa9ee60a7
Arthur Cavalleiro <98990206+Arthur-Cavalleiro@users.noreply.github.com>
2024-04-15 22:00:16 -0300

    adicionado equivalencia program.c

--- program.c
+++ program.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 /*
   A: N = entrada
   B: resultado = 1
@@ -35,6 +33,10 @@ inicio:
     7: Se i <= n então va_para 8 senão va_para 5
     8: Faça F va_para Rx(✔)
     Rx: print(error)
+
+    Considerando n = 2
+    (1,e)(2,e)(3,e)(4,B)(5,BC)(6,BCD)(7,BCDE)(5,BCDE)(6,BCDED)(7,BCDEDE)(8,BCDEDEF)(✔)
+  
   */
 }
 
@@ -68,6 +70,10 @@ int fatorialIterativo(int n) {
     7: Faça E va_para 5
     8: Faça F va_para Rx(✔)
     Rx: print(error)
+
+    Considerando n = 2
+    (1,e)(2,e)(3,e)(4,B)(5,BC)(6,BC)(7,BCD)(8,BCDE)(6,BCDE)(7,BCDED)(8,BCDEDE)(6,BCDEDE)(9,BCDEDEF)(✔)
+    
   */
 }
 
@@ -93,6 +99,9 @@ int fatorialRecursivo(int n) {
     5: Faça C va_para 6
     6: Completo Rx(✔)
     Rx: print(error)
+
+    Considerando n = 2
+    (1,e)(2,e)(3,e)(4,e)(5,B)(1,B)(2,B)(3,B)(4,B)(5,BB)(1,BB)(2,BB)(3,BB)(4,BB)(6,BBC)(7,✔)
   */
 }
 
